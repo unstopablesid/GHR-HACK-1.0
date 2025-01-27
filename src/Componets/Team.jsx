80f1afae679b8b03c34fc0cbb936d088da3c8df5
unstopablesid <mayurnikumbh2004@gmail.com>
2025-01-27 18:23:45 +0530

    Comment out team member entries in Team component for future reference

--- src/Componets/Team.jsx
+++ src/Componets/Team.jsx
@@ -19,7 +19,7 @@ const teamMembers = [
     { name: "Kalpesh Borde", role: "Overall Coordinator", photo: Kalpesh, linkedin: "https://www.linkedin.com/in/kalpesh-borde-0b1b3b1b7",Logo: Logo  },
     { name: "Vidya Baviskar", role: "Overall Coordinator", photo: Vidya ,Logo: Logo, linkedin: "https://www.linkedin.com/in/vidya-baviskar-0b1b3b1b7" },
     { name: "Mrunal Mahajan", role: "Overall Coordinator", photo: Mrunal,Logo: Logo ,linkedin: "https://www.linkedin.com/in/mrunal-mahajan-0b1b3b1b7" },
-    { name: "Mayur Nikumbh", role: "Technical Head", photo: Mayur ,Logo: Logo ,linkedin: "https://www.linkedin.com/in/mayur-nikumbh"},
+    // { name: "Mayur Nikumbh", role: "Technical Head", photo: Mayur ,Logo: Logo ,linkedin: "https://www.linkedin.com/in/mayur-nikumbh"},
     // { name: "Neeraj Rakhecha", role: "Design Head", photo: Jain, linkedin: "https://www.linkedin.com/in/rohit-pitrubhakt-5a2737257" },
     // { name: "Kartik deshmukh", role: "Design Head", photo: Kartik2, linkedin: "https://www.linkedin.com/in/rohit-pitrubhakt-5a2737257" },
     // { name: "Bhavesh Kanhaiya", role: "Marketing Head", photo: Bhavesh ,Logo: Logo ,linkedin: "https://www.linkedin.com/in/bhavesh-kanhaiya-0b1b3b1b7" },
