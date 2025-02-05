317251130ddbc8e7f80dc55b342006049f995741
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-02-06 00:28:19 +0530

    Update team member LinkedIn profiles and correct Aditi's photo path

--- src/Componets/Team.jsx
+++ src/Componets/Team.jsx
@@ -5,7 +5,7 @@ import Kalpesh from "../assets/Team/Kalpesh-borde.jpg";
 import Mrunal from "../assets/Team/Mrunal-Mahajan.jpg";
 import Kartik from "../assets/Team/kartik-Chaudhari.jpg";
 import Logo from "../assets/Logo/Linked.png";
-import Aditi from "../assets/Team/Aditi Patil.jpg";
+import Aditi from "../assets/Team/Aditi Patil2.jpg";
 import Anushka from "../assets/Team/Anushka Anasane.jpg";
 import Bhavesh from "../assets/Team/Bhavesh Chaudhari.jpg";
 import Bhaveshk from "../assets/Team/Bhavesh-kanaiya.jpg";
@@ -52,45 +52,45 @@ const teamData = {
   Technical: [
     { name: "Mayur Nikumbh", role: "Team Leader", photo: Mayur, linkedin: "#" },
     { name: "Lokesh Salunkhe", role: "Member", photo: Lokesh, linkedin: "#" },
-    { name: "Khushi Chari", role: "Member", photo: Khushi, linkedin: "#" },
-    { name: "Omkar Dolhare", role: "Member", photo: Omkaar, linkedin: "#" },
+    { name: "Khushi Chari", role: "Member", photo: Khushi, linkedin: "https://www.linkedin.com/in/khushichhari" },
+    { name: "Omkar Dolhare", role: "Member", photo: Omkaar, linkedin: "https://www.linkedin.com/in/omkar-dolhare-24760b2b1?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
     { name: "Himanshu Kawle", role: "Member", photo: Himanshu, linkedin: "#" },
-    { name: "Mohit Sharma", role: "Member", photo: Mohit, linkedin: "#" },
+    { name: "Mohit Sharma", role: "Member", photo: Mohit, linkedin: "https://www.linkedin.com/in/mr-mohit-sharma-541167297?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
 
   ],
   Design: [
-    { name: "Neeraj Jain", role: "Design Leader", photo: Neeraj, linkedin: "#" },
+    { name: "Neeraj Rakhecha", role: "Design Leader", photo: Neeraj, linkedin: "https://www.linkedin.com/in/neeraj-rakhecha-084b85316" },
     { name: "Kartik Deshmukh", role: "Co-leader", photo: KartikD, linkedin: "#" },
-    { name: "Bhumika Ingle", role: "Member", photo: Bhumika, linkedin: "#" },
-    { name: "Anushka", role: "Member", photo: Anushka, linkedin: "#" },
+    { name: "Bhumika Ingle", role: "Member", photo: Bhumika, linkedin: "www.linkedin.com/in/bhumika-ingale-83209531b" },
+    { name: "Anushka", role: "Member", photo: Anushka, linkedin: "https://www.linkedin.com/in/anushka-anasane-33202629b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
 ],
 Marketing: [
-  { name: "Bhavesh Kanhaiya", role: "Team leader", photo: Bhaveshk, linkedin: "#" },
-  { name: "Shivani Deshpande", role: "Co-leader", photo: Shivani, linkedin: "#" },
+  { name: "Bhavesh Kanhaiya", role: "Team leader", photo: Bhaveshk, linkedin: "https://www.linkedin.com/in/bhavesh-kanhaiya-689b65217?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+  { name: "Shivani Deshpande", role: "Co-leader", photo: Shivani, linkedin: "https://www.linkedin.com/in/shivani-deshpande-507355341?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
   { name: "Aditi patil", role: "Member", photo: Aditi, linkedin: "#" },
   { name: "Lokesh Salunkhe", role: "Member", photo: Lokesh2, linkedin: "#" },
-  { name: "Purva Tiwari", role: "Member", photo: Purva, linkedin: "#" },
-  { name: "Leena chaudhari", role: "Member", photo: Leena, linkedin: "#" },
-  { name: "Sakshi Nimbalkar", role: "Member", photo: Sakshi, linkedin: "#" },
-  { name: "Prathmesh Misal", role: "Member", photo: Prathmesh, linkedin: "#" },
-  { name: "Bhavesh Chaudhari", role: "Member", photo: Bhavesh, linkedin: "#" },
+  { name: "Purva Tiwari", role: "Member", photo: Purva, linkedin: "https://www.linkedin.com/in/purva-tiwari-4b97b1297?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+  { name: "Leena chaudhari", role: "Member", photo: Leena, linkedin: "https://www.linkedin.com/in/leena-chaudhari-a14b17342?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+  { name: "Sakshi Nimbalkar", role: "Member", photo: Sakshi, linkedin: "https://www.linkedin.com/in/sakshi-nimbalkar-7770532a2" },
+  { name: "Prathmesh Misar", role: "Member", photo: Prathmesh, linkedin: "#" },
+  { name: "Bhavesh Chaudhari", role: "Member", photo: Bhavesh, linkedin: "https://www.linkedin.com/in/bhavesh-chaudhari-10737b31b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
 
 ],
   Finance: [
-    { name: "Shreyash Badgujar", role: "Team leader", photo: ShreyadhB, linkedin: "#" },
+    { name: "Shreyash Badgujar", role: "Team leader", photo: ShreyadhB, linkedin: "https://www.linkedin.com/in/shreyas-badgujar-a97058258" },
     { name: "Rohit Pitrubhakt", role: "Member", photo: Rohit, linkedin: "#" },
-    { name: "Gautami Upasani", role: "Member", photo: Gautami, linkedin: "#" },
+    { name: "Gautami Upasani", role: "Member", photo: Gautami, linkedin: "https://www.linkedin.com/in/gautami-upasani-2027962b2" },
   ],
   Logistic: [
-    { name: "Krunal Badgujar", role: "Team Leader", photo: Krunal, linkedin: "#" },
-    { name: "Jayesh Mahajan", role: "Member", photo: Jayesh, linkedin: "#" },
-    { name: "Gaurav mahajan", role: "Member", photo: Ghaurav, linkedin: "#" },
-    { name: "Shreyash Ahhirao", role: "Member", photo: Shreyash, linkedin: "#" },
+    { name: "Krunal Badgujar", role: "Team Leader", photo: Krunal, linkedin: "https://www.linkedin.com/in/krunal-badgujar-45b3a125b?" },
+    { name: "Jayesh Mahajan", role: "Member", photo: Jayesh, linkedin: "https://www.linkedin.com/in/jayesh-mahajan-9b4ba2278?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+    { name: "Gaurav mahajan", role: "Member", photo: Ghaurav, linkedin: "https://www.linkedin.com/in/gaurav-mahajan-8663a3258" },
+    { name: "Shreyash Ahhirao", role: "Member", photo: Shreyash, linkedin: "https://www.linkedin.com/in/shreyash-ahirrao-865a47288?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
   ],
   Operations: [
     { name: "Yashraj Patil", role: "Team Leader", photo: Yash, linkedin: "#" },
-    { name: "Shravni Kasar", role: "Member", photo: Shravni, linkedin: "#" },
-    { name: "Disha Suryavanshi", role: "Member", photo: Disha, linkedin: "#" },
+    { name: "Shravni Kasar", role: "Member", photo: Shravni, linkedin: "https://www.linkedin.com/in/shravani-kasar-5a5623258" },
+    { name: "Disha Suryavanshi", role: "Member", photo: Disha, linkedin: "https://www.linkedin.com/in/disha-suryawanshi-8a5401345" },
     { name: "Litesh Nannaware ", role: "Member", photo: Litesh, linkedin: "#" },
   ],
 };
